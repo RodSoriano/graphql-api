d127125fba84947ee746f5e2c2c2f5d8c8ac51a7
RodSoriano <rrecinos@applaudostudios.com>
2022-12-25 01:44:21 -0600

    Create area factory

---
+++ database/factories/AreaFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Area;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Area>
+ */
+class AreaFactory extends Factory
+{
+    public function definition(): array
+    {
+        return [
+            'crag_id' => fake()->randomDigit(),
+            'name' => fake()->streetSuffix(),
+            'location' => fake()->streetAddress(),
+            'route_count' => fake()->numberBetween(1, 10),
+            'description' => fake()->sentence(),
+            'detail' => [],
+        ];
+    }
+}
