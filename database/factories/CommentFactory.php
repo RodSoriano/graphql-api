ea6b2e85384c9997bb2125e8eebd5b897ed1d6f8
RodSoriano <rrecinos@applaudostudios.com>
2022-12-25 01:45:10 -0600

    Create comment factory

---
+++ database/factories/CommentFactory.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Comment;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Comment>
+ */
+class CommentFactory extends Factory
+{
+    public function definition(): array
+    {
+        return [
+            'commentable_type' => fake()->word(),
+            'commentable_id' => fake()->randomNumber(),
+            'comment' => fake()->sentence(),
+        ];
+    }
+}
