89ba96b386d9f1b0fb29f2c9c26287c1f43a5188
RodSoriano <rrecinos@applaudostudios.com>
2022-12-28 11:46:36 -0600

    Update seeder so it also creates reviews for model

--- database/seeders/RouteSeeder.php
+++ database/seeders/RouteSeeder.php
@@ -3,12 +3,23 @@
 namespace Database\Seeders;
 
 use App\Models\Route;
+use App\Models\TechnicalReview;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Seeder;
 
 class RouteSeeder extends Seeder
 {
     public function run(int $quantity = 20): void
     {
-        Route::factory($quantity)->hasComments(1)->create();
+        $routes = Route::factory($quantity)->hasComments(1)->create();
+
+        $this->reviewsCreator($routes, $quantity);
+    }
+
+    private function reviewsCreator(Collection $routes, int $quantity): void
+    {
+        TechnicalReview::factory($quantity)
+            ->recycle($routes)
+            ->create();
     }
 }
