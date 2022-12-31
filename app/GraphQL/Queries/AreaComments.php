b32b9d08536c31333d9d0f37385c76a0adf7e070
RodSoriano <rrecinos@applaudostudios.com>
2022-12-31 12:22:19 -0600

    Create new query to get comments of an area model

---
+++ app/GraphQL/Queries/AreaComments.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\GraphQL\Queries;
+
+use App\Models\Area;
+use Illuminate\Database\Eloquent\Collection;
+
+final class AreaComments
+{
+    public function __invoke(mixed $_, array $args): Collection
+    {
+        return Area::find($args['id'])->comments;
+    }
+}
