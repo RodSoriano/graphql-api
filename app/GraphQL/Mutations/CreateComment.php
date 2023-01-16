1cd5fb92355d6f4aa8926e90eef33e30a2a4d7d2
RodSoriano <rrecinos@applaudostudios.com>
2023-01-16 00:06:43 -0600

    Update with validation if class doesn't exist

--- app/GraphQL/Mutations/CreateComment.php
+++ app/GraphQL/Mutations/CreateComment.php
@@ -3,18 +3,34 @@
 namespace App\GraphQL\Mutations;
 
 use App\Models\Comment;
+use Exception;
 use Illuminate\Database\Eloquent\Model;
 
 final class CreateComment
 {
-    protected const MODEL_NAMESPACE = "App\\Models\\";
+    private const MODEL_NAMESPACE = "App\\Models\\";
 
-    public function __invoke(mixed $_, array $args): Model
+    public function __invoke(mixed $_, array $args): Model|Exception
     {
         $model = self::MODEL_NAMESPACE . $args['commentable_type'];
+
+        $id = $args['commentable_id'];
+
+        $this->modelExist($model, $id);
 
         $args['commentable_type'] = $model;
 
         return Comment::create($args);
+    }
+
+    private function modelExist(string $model, int $id): void
+    {
+        $modelInstance = new $model();
+
+        $record = $modelInstance->find($id);
+
+        if ($record === null) {
+            throw new Exception("Place not found.");
+        }
     }
 }
