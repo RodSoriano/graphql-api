24931da2ba0f5907248986ee8f77868231a23c18
RodSoriano <rrecinos@applaudostudios.com>
2023-02-10 12:24:27 -0600

    Create filter resolvers for route model

--- app/GraphQL/Queries/Routes.php
+++ app/GraphQL/Queries/Routes.php
@@ -9,10 +9,55 @@ final class Routes
 {
     public function __invoke(mixed $_, array $args): Collection|Route
     {
+        $data = null;
+
+        if ($args['grade']) {
+            $data = $this->gradeFilter($args);
+        }
+
+        if ($args['quick_draws']) {
+            $data = $this->quickDrawFilter($args);
+        }
+
+        if ($args['type']) {
+            $data = $this->typeFilter($args);
+        }
+
+        if ($data) {
+            return $data;
+        }
+
+        return $this->modelOrCollection($args);
+    }
+
+    private function modelOrCollection(array $args): Collection|Route
+    {
         if ($args['id']) {
             return Route::where('id', $args['id'])->get();
         }
 
         return Route::all();
+    }
+
+    private function gradeFilter(array $args): Collection|Route
+    {
+        if ($args['grade']) {
+            return Route::where('grade', $args['grade'])->get();
+        }
+    }
+
+    private function quickDrawFilter(array $args): Collection|Route
+    {
+        if ($args['quick_draws']) {
+            return Route::where('quick_draw_count', $args['quick_draws'])
+                ->get();
+        }
+    }
+
+    private function typeFilter(array $args): Collection|Route
+    {
+        if ($args['type']) {
+            return Route::where('type', $args['type'])->get();
+        }
     }
 }
