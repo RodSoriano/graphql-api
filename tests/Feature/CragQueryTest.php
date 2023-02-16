1e22b212889693922ce15148dcd3d6175bad1876
RodSoriano <rrecinos@applaudostudios.com>
2023-02-15 22:02:21 -0600

    Create test for crags query

--- tests/Feature/CragQueryTest.php
+++ tests/Feature/CragQueryTest.php
@@ -10,7 +10,7 @@ use Tests\TestCase;
 class CragQueryTest extends TestCase
 {
     /** @test */
-    public function it_returns_the_collection_of_crag_model(): void
+    public function it_queries_the_collection_of_crag_model(): void
     {
         $expected = Crag::all();
 
@@ -21,10 +21,24 @@ class CragQueryTest extends TestCase
         $this->assertEquals($expected->toArray(), $actual);
     }
 
+    /** @test */
+    public function it_queries_a_single_crag_model(): void
+    {
+        $expected = Crag::query()
+            ->where('id', '=', 2)
+            ->get();
+
+        $response = $this->cragQuery();
+
+        $actual = $response['data']['crags'];
+
+        $this->assertEquals($expected->toArray(), $actual);
+    }
+
     private function cragsQuery(): TestResponse
     {
         return $this->graphQL(
-            /** @lang GraphQL */ '
+        /** @lang GraphQL */ '
             {
                 crags {
                     id
@@ -36,6 +50,26 @@ class CragQueryTest extends TestCase
                     detail
                 }
             }
-        ');
+        '
+        );
+    }
+
+    private function cragQuery(): TestResponse
+    {
+        return $this->graphQL(
+        /** @lang GraphQL */ '
+            {
+                crags(id: 2) {
+                    id
+                    name
+                    location
+                    route_count
+                    get_here
+                    description
+                    detail
+                }
+            }
+        '
+        );
     }
 }
